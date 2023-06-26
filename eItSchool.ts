9b8574ffed1fa399e4b122da7c4d0cfd951e3ead
Olga Bouin <olga.bouin@adeo.com>
2023-06-26 19:48:04 +0200

    feat: add methods to classes

--- eItSchool.ts
+++ eItSchool.ts
@@ -1,11 +1,18 @@
 class School {
     constructor(public name: string) {}
     private _trainings: Training[] = [];
+    resume: string;
+    creationDate: Date;
+
+    public getSchoolDescription() {
+        return `School ${this.name} created the ${this.creationDate} is ${this.resume} proposes such trainings as: ${this._trainings}`;
+    }
 }
 
 class Training {
     constructor(public name: string) {}
     type: TrainingType;
+    courses: Course[];
 }
 
 enum TrainingType {
@@ -15,7 +22,18 @@ enum TrainingType {
 }
 
 class Course {
-    constructor(public name: string, public type: TrainingType) {}
+    private _animator: Employee;
+    mainTraining: Training;
+    otherTrainings: Training[];
+    resources: EducationalResources[];
+    subscribers: Subscriber[];
+    constructor(public name: string, public type: TrainingType, animator: Employee) {
+        this._animator = animator;
+    }
+
+    public subscribe(student: Subscriber) {
+        this.subscribers.push(student)
+    }
 }
 
 class Person {
@@ -67,11 +85,16 @@ class Contact {
 }
 
 abstract class EducationalResources {
+    protected constructor(name: string) {
+    }
 
 }
 
 class Video extends EducationalResources {
-    type: VideoType
+    type: VideoType;
+    constructor(name: string) {
+        super(name);
+    }
 }
 
 enum VideoType {
@@ -80,15 +103,30 @@ enum VideoType {
 }
 
 class Homework extends EducationalResources {
-
+    dueDate: Date;
+    course: Course;
+    student: Subscriber;
+    constructor(name: string) {
+        super(name);
+    }
 }
 
 class Project extends EducationalResources {
+    theme: string;
+    student: Subscriber;
+    tutor: Employee;
+
+    constructor(name: string) {
+        super(name);
+    }
 
 }
 
 class Exam extends EducationalResources {
     type: ExamType;
+    constructor(name: string) {
+        super(name);
+    }
 }
 
 enum ExamType {
